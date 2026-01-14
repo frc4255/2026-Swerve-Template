3bd28bb49b8f9698a6bfda4c83f82f5aec8f4724
kai-slothz <slothzngl@gmail.com>
2026-01-13 16:16:46 -0800

    Initial commit

--- src/main/java/frc/robot/FieldLayout.java
+++ src/main/java/frc/robot/FieldLayout.java
@@ -1,5 +1,39 @@
 package frc.robot;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.Unit;
+
 public class FieldLayout {
-    
+    public static final double FIELD_WIDTH = Units.inchesToMeters(317.69);
+    public static final double FIELD_LENGTH = Units.inchesToMeters(651.22);
+
+    public static final double startingLine = Units.inchesToMeters(156.61);
+
+    public static final class hub{
+        public static final double HUB_WIDTH = Units.inchesToMeters();
+    }
+
+
+
+ 
+
+
+
+
+
+
+
 }
